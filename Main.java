e8471b045dcf358456a3f8d0a09582eeab35feea
neilmars10 <neilm.1301@gmail.com>
2024-05-21 11:16:43 -0700

    Created Main.java and ScheduleGUI.java

---
+++ Main.java
@@ -0,0 +1,17 @@
+/*
+* Programmer(s): Kaden Bautista and Neil Marquez
+* Date: 5/31/24
+* Section: Period 2
+* Assignment: White Tower Project
+*/
+
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args) {
+
+
+
+
+
+    }
+}
